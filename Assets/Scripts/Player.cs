7887838f66c8cc89ca5cb10196c2cf5f714304d0
Chimbinhaa <ianrlima08@gmail.com>
2026-09-22 09:25:35 -0300

    Limitei o pulo
    limitei o pulo para o player conseguir pular apenas quando tiver no chão

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,39 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
+    private bool isGrounded = false;
     void Start()
     {
-     rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(new Vector2 (0f, 5f), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
+
